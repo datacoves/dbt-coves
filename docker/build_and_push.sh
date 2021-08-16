0f47fb56465e9d80d1819dc3fb27356127c025b5
Sebastian Sassi <ssassi@sophilabs.com>
2021-08-16 18:28:46 -0300

    dbt-coves docker image update.

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-docker build -t datacoves/dbt-coves:latest .
-docker build -t datacoves/dbt-coves:0.20.0-a.3 .
+if [ -z "$1" ]
+  then
+    echo "No dbt version supplied, i.e. 0.19.2"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No dbt-coves version supplied, i.e. 0.20.0a3"
+    exit 1
+fi
+
+docker build -t datacoves/dbt-coves:latest --build-arg DBT_VERSION="$1" --build-arg DBT_COVES_VERSION="$2" .
+docker build -t datacoves/dbt-coves:"$1"-"$2" --build-arg DBT_VERSION="$1" --build-arg DBT_COVES_VERSION="$2" .
 docker push datacoves/dbt-coves:latest
-docker push datacoves/dbt-coves:0.20.0-a.3
+docker push datacoves/dbt-coves:"$1"-"$2"
 
 echo "You can now 'run docker run -it -v $PWD:/usr/app -v /path/to/your/profiles.yml:/root/.dbt/profiles.yml datacoves/dbt-coves check'"
