9994feabbaf5831119c197811733c2e4bb4201b8
Bruno Antonellini <antonellinibruno@gmail.com>
2022-03-30 15:47:01 -0300

    New dbt-coves setup ssh behaviour
    - If key already exists, inform the public key to configure on Git
    - If key not found, prompt the user to provide it or generate a new one
    - Replaced both situations to search for ecdsa keys instead of previous rsa ones.

--- dbt_coves/tasks/setup/ssh.py
+++ dbt_coves/tasks/setup/ssh.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from rich.console import Console
 
 from dbt_coves.tasks.base import NonDbtBaseTask
+from dbt_coves.utils.shell import shell_run
 from .utils import print_row
 
 
@@ -32,25 +33,58 @@ class SetupSSHTask(NonDbtBaseTask):
     @classmethod
     def run(cls) -> int:
         ssh_status = "[red]MISSING[/red]"
-        key_path = "~/.ssh/id_rsa"
+        key_path = "~/.ssh/id_ecdsa"
+        public_key_path = "~/.ssh/id_ecdsa.pub"
         key_path_abs = Path(key_path).expanduser()
+        public_key_path_abs = Path(public_key_path).expanduser()
         ssh_exists = key_path_abs.exists()
+
         if ssh_exists:
             ssh_status = "[green]FOUND :heavy_check_mark:[/green]"
-        print_row(f"Checking for key in '{key_path}'", ssh_status, new_section=True)
-        confirmed = questionary.confirm(
-            "Would you like to create a new SSH key?"
-            if not ssh_exists
-            else "Woud you like to overwrite your existing SSH key?",
-            default=not ssh_exists,
-        ).ask()
-        if confirmed:
-            ssh_key = questionary.text("Please paste your SSH key:").ask()
-            key_path_abs.parent.mkdir(parents=True, exist_ok=True)
-            with open(key_path_abs, "w") as file:
-                file.write(ssh_key)
-            os.chmod(key_path_abs, 0o600)
-            console.print(
-                f"[green]:heavy_check_mark: New SSH key stored on '{key_path}'[/green]"
+            print_row(
+                f"Checking for key in '{key_path}'", ssh_status, new_section=False
             )
+            cls.output_public_key(public_key_path_abs)
+
+        if not ssh_exists:
+            print_row(
+                f"Checking for key in '{key_path}'", ssh_status, new_section=False
+            )
+            action = (
+                questionary.select(
+                    "Would you like to provide your existent private SSH key or generate a new one?",
+                    choices=["Provide", "Generate"],
+                )
+                .ask()
+                .lower()
+            )
+            if action == "provide":
+                ssh_key = questionary.text("Please paste your private SSH key:").ask()
+                key_path_abs.parent.mkdir(parents=True, exist_ok=True)
+                with open(key_path_abs, "w") as file:
+                    file.write(ssh_key)
+                os.chmod(key_path_abs, 0o600)
+                console.print(
+                    f"[green]:heavy_check_mark: New SSH key stored on '{key_path}'[/green]"
+                )
+                cls.output_public_key(public_key_path_abs)
+            if action == "generate":
+                output = cls.generate_ecdsa_keys(key_path_abs)
+                if output.returncode == 0:
+                    console.print(
+                        f"[green]:heavy_check_mark: SSH ecdsa key generated on '{key_path}'[/green]"
+                    )
+                    cls.output_public_key(public_key_path_abs)
+
         return 0
+
+    @classmethod
+    def generate_ecdsa_keys(cls, key_path_abs):
+        shell_run(args=["ssh-keygen", "-q", "-t", "ecdsa", "-f", key_path_abs])
+
+    @classmethod
+    def output_public_key(cls, public_key_path_abs):
+        console.print(
+            f"Please configure the following public key in your Git server (Gitlab, Github, Bitbucket, etc):"
+        )
+        print(open(public_key_path_abs, "r").read())
