dc2866e6a0bbb96104665cb91372de0b020fde63
Bruno Antonellini <antonellinibruno@gmail.com>
2022-04-06 15:05:17 -0300

    Cleaning git space

--- publish.sh
+++ publish.sh
@@ -4,7 +4,24 @@
 
 # poetry run towncrier build
 
+#major
+#minor
+#patch
+#release
+#build
+
+TYPE=$1
+
+if [ $TYPE = 'major' ] || [ $TYPE = 'minor' ] || [ $TYPE = 'patch' ] || [ $TYPE = 'release' ] || [ $TYPE = 'build' ]
+then
+    echo "Bumping $TYPE version"
+    bumpversion $TYPE
+else
+    echo "$TYPE invalid version type"
+fi
+
 # search and replace <current version> by <new version>, i.e. 1.0.4-a.3 by 1.0.4-a.4
+
 # bumpversion <new_version>
-poetry build
-poetry publish
+#poetry build
+#poetry publish
