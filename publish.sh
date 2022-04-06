b40de207fdc98f9b78cd29ce0a4bf5119c62f474
Bruno Antonellini <antonellinibruno@gmail.com>
2022-04-06 15:38:28 -0300

    Changes to publish.sh

--- publish.sh
+++ publish.sh
@@ -3,25 +3,23 @@
 # Run `poetry run towncrier create 123.feature` to update changelog
 
 # poetry run towncrier build
+die () {
+    echo >&2 "$@"
+    exit 1
+}
 
-#major
-#minor
-#patch
-#release
-#build
+[ "$#" -eq 1 ] || die "1 argument (version type) required, $# provided"
+[ $1 = 'major' ] || 
+[ $1 = 'minor' ] || 
+[ $1 = 'patch' ] || 
+[ $1 = 'release' ] || 
+[ $1 = 'build' ] || 
+die "version type required: (major, minor, patch, release, build), $1 provided"
+
 
 TYPE=$1
+# bumpversion -> search and replace versions, i.e. 1.0.4-a.3 by 1.0.4-a.4
+bumpversion $TYPE
+poetry build
+poetry publish
 
-if [ $TYPE = 'major' ] || [ $TYPE = 'minor' ] || [ $TYPE = 'patch' ] || [ $TYPE = 'release' ] || [ $TYPE = 'build' ]
-then
-    echo "Bumping $TYPE version"
-    bumpversion $TYPE
-else
-    echo "$TYPE invalid version type"
-fi
-
-# search and replace <current version> by <new version>, i.e. 1.0.4-a.3 by 1.0.4-a.4
-
-# bumpversion <new_version>
-#poetry build
-#poetry publish
