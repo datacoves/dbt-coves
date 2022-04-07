deb9cedab061fd00c466a947c3ad1e060ffbb708
Bruno Antonellini <antonellinibruno@gmail.com>
2022-04-07 12:03:48 -0300

    Defaulting empty version type to build

--- publish.sh
+++ publish.sh
@@ -3,23 +3,25 @@
 # Run `poetry run towncrier create 123.feature` to update changelog
 
 # poetry run towncrier build
-die () {
+die() {
     echo >&2 "$@"
     exit 1
 }
 
-[ "$#" -eq 1 ] || die "1 argument (version type) required, $# provided"
-[ $1 = 'major' ] || 
-[ $1 = 'minor' ] || 
-[ $1 = 'patch' ] || 
-[ $1 = 'release' ] || 
-[ $1 = 'build' ] || 
-die "version type required: (major, minor, patch, release, build), $1 provided"
+if [ "$#" -eq 0 ]; then
+    TYPE='build'
+elif
+    [ $1 = 'major' ] ||
+        [ $1 = 'minor' ] ||
+        [ $1 = 'patch' ] ||
+        [ $1 = 'release' ] ||
+        [ $1 = 'build' ]
+then
+    TYPE=$1
+else
+    die "version type required: (major, minor, patch, release, build), $1 provided"
 
-
-TYPE=$1
-# bumpversion -> search and replace versions, i.e. 1.0.4-a.3 by 1.0.4-a.4
+fi
 bumpversion $TYPE
 poetry build
 poetry publish
-
