711b50a012daefa4438d5a4b4701afbeff641ae0
Bruno Antonellini <antonellinibruno@gmail.com>
2022-06-15 13:17:37 -0300

    Added --open_ssl_public_key flag to 'all' and 'ssh' setups for OpenSSL PUB generation

--- dbt_coves/utils/shell.py
+++ dbt_coves/utils/shell.py
@@ -20,6 +20,10 @@ def run_and_capture(args_list: List) -> CompletedProcess:
     return shell_run(args_list, capture_output=True, text=True)
 
 
+def run_and_capture_shell(args_list):
+    return shell_run(args_list, shell=True)
+
+
 def run_and_capture_cwd(args_list, cwd) -> CompletedProcess:
     return shell_run(args_list, cwd=cwd)
 
