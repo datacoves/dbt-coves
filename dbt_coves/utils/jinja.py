d8c5f737dd6053596f7ee9cdbb6703dbb55687d9
Zhao Wang <zhawang@purestorage.com>
2022-11-14 16:25:27 -0800

    Allow using environment variables in jinja template through env_vars['{var_name}']

--- dbt_coves/utils/jinja.py
+++ dbt_coves/utils/jinja.py
@@ -1,4 +1,14 @@
+import copy
+import os
+
 from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader
+
+
+def add_env_vars(context):
+    context_with_env_vars = copy.deepcopy(context)
+    for env_var_key in os.environ.keys():
+        context_with_env_vars['env_var'][env_var_key] = os.environ[env_var_key]
+    return context_with_env_vars
 
 
 def render_template_file(name, context, output_path, templates_folder=".dbt_coves/templates"):
@@ -7,7 +17,8 @@ def render_template_file(name, context, output_path, templates_folder=".dbt_cove
         keep_trailing_newline=True,
     )
     template = env.get_template(name)
-    output = template.render(**context)
+    context_with_env_vars = add_env_vars(context)
+    output = template.render(**context_with_env_vars)
 
     with open(output_path, "w") as rendered:
         rendered.write(output)
@@ -17,7 +28,8 @@ def render_template_file(name, context, output_path, templates_folder=".dbt_cove
 
 def render_template(template_content, context):
     template = Environment().from_string(template_content)
-    return template.render(**context)
+    context_with_env_vars = add_env_vars(context)
+    return template.render(**context_with_env_vars)
 
 
 def get_render_output(name, context, templates_folder=".dbt_coves/templates"):
@@ -26,6 +38,7 @@ def get_render_output(name, context, templates_folder=".dbt_coves/templates"):
         keep_trailing_newline=True,
     )
     template = env.get_template(name)
-    output = template.render(**context)
+    context_with_env_vars = add_env_vars(context)
+    output = template.render(**context_with_env_vars)
 
     return output
