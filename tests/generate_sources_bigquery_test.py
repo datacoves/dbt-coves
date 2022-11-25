033357b551ac34c69cfd7d5bb3eb2553177db57e
Santiago Menendez <santiagom@datacoves.com>
2022-11-25 10:20:35 -0300

    Added bigquery tests

--- tests/generate_sources_bigquery_test.py
+++ tests/generate_sources_bigquery_test.py
@@ -59,7 +59,6 @@ def test_generate_test_model():
     rows = []
     assert query_job.errors == None
     for row in query_job:
-        print(row)
         rows.append(row)
     assert len(rows) == 1
 
@@ -83,112 +82,123 @@ def test_generate_sources_bigquery():
         "update",
         "--verbose",
     ]
-    print(" ".join(command))
+    
     # Execute CLI command and interact with it
     process = subprocess.run(args=command, input="\n", encoding="utf-8")
 
     assert process.returncode == 0
     assert os.path.isdir("models") == True
 
-    #with open(metadata_file, "r") as file:
-    #    metadata_csv = csv.reader(file, delimiter=",")
-    #    next(metadata_csv)
+    with open(metadata_file, "r") as file:
+        metadata_csv = csv.reader(file, delimiter=",")
+        next(metadata_csv)
 
-    #    schema_yml = None
-    #    test_model_props = None
-    #    test_model_sql = None
+        schema_yml = None
+        test_model_props = None
+        test_model_sql = None
 
-    #    # Check if schema.yml and test_table.yml exists
-    #    for file in os.listdir(f"models/staging/{schema}"):
-    #        if file == f"{schema}.yml":
-    #            with open(f"models/staging/{schema}/{file}", "r") as file:
-    #                try:
-    #                    schema_yml = yaml.safe_load(file)
-    #                except yaml.YAMLError as err:
-    #                    print(err)
-    #                    assert False
-    #            continue
+        dataset_folder = dataset_id.lower()
 
-    #        if file == f"{test_table}.yml":
-    #            with open(f"models/staging/{schema}/{file}", "r") as file:
-    #                try:
-    #                    test_model_props = yaml.safe_load(file)
-    #                except yaml.YAMLError as err:
-    #                    print(err)
-    #                    assert False
-    #            continue
+        # Check if schema.yml and test_table.yml exists
+        for file in os.listdir(f"models/staging/{dataset_folder}"):
+            if file == f"{dataset_folder}.yml":
+                with open(f"models/staging/{dataset_folder}/{file}", "r") as file:
+                    try:
+                        schema_yml = yaml.safe_load(file)
+                    except yaml.YAMLError as err:
+                        print(err)
+                        assert False
+                continue
 
-    #        if file == f"{test_table}.sql":
-    #            # TODO: Read SQL File
-    #            with open(f"models/staging/{schema}/{file}", "r") as file:
-    #                test_model_sql = file.read()
-    #            continue
+            if file == f"{test_table}.yml":
+                with open(f"models/staging/{dataset_folder}/{file}", "r") as file:
+                    try:
+                        test_model_props = yaml.safe_load(file)
+                    except yaml.YAMLError as err:
+                        print(err)
+                        assert False
+                continue
 
-    #    # Validate test_table.sql
-    #    assert test_model_sql != None
+            if file == f"{test_table}.sql":
+                # TODO: Read SQL File
+                with open(f"models/staging/{dataset_folder}/{file}", "r") as file:
+                    test_model_sql = file.read()
+                continue
 
-    #    # Validate schema.yml
-    #    assert schema_yml != None
-    #    assert schema_yml["sources"][0]["name"] == schema
-    #    assert schema_yml["sources"][0]["database"] == database
-    #    found_table = False
-    #    for tables in schema_yml["sources"][0]["tables"]:
-    #        if tables["name"] == test_table:
-    #            found_table = True
-    #            break
-    #    assert found_table
+        # Validate test_table.sql
+        assert test_model_sql != None
 
-    #    # Validate test_table.yml with metadata.csv
-    #    assert test_model_props != None
-    #    assert test_model_props["models"][0]["name"] == test_table
-    #    for row in metadata_csv:
-    #        # "database","schema","relation","column","key","type","description"
-    #        assert row[0] == database
-    #        assert row[1] == schema
-    #        assert row[2] == test_table
-    #        # Delete underscore and get row name
-    #        # Validate if is a key (JSON Flatten)
-    #        if row[4] != "":
-    #            row_name = row[4]
-    #        elif row[3][0] == "_":
-    #            row_name = row[3][1:]
-    #        else:
-    #            row_name = row[3]
-    #        found_col = False
-    #        for col in test_model_props["models"][0]["columns"]:
-    #            if col["name"] == row_name:
-    #                found_col = True
-    #                assert col["description"] == row[6]
-    #                break
-    #        assert found_col
+        # Validate schema.yml
+        assert schema_yml != None
+        assert schema_yml["sources"][0]["name"] == dataset_id.lower()
+        
+        #assert schema_yml["sources"][0]["database"] == project_id
+        
+        found_table = False
+        for tables in schema_yml["sources"][0]["tables"]:
+            if tables["name"] == test_table:
+                found_table = True
+                break
+        assert found_table
 
-    ## Convert SQL to Jinja2 Template and replace function
-    #def source(schema, table):
-    #    return f"{schema}.{table}"
+        # Validate test_table.yml with metadata.csv
+        assert test_model_props != None
+        assert test_model_props["models"][0]["name"] == test_table
+        for row in metadata_csv:
+            # "database","schema","relation","column","key","type","description"
+            assert row[0] == project_id
+            assert row[1] == dataset_id
+            assert row[2] == test_table
+            # Delete underscore and get row name
+            # Validate if is a key (JSON Flatten)
+            if row[4] != "":
+                row_name = row[4]
+            elif row[3][0] == "_":
+                row_name = row[3][1:]
+            else:
+                row_name = row[3]
+            found_col = False
+            for col in test_model_props["models"][0]["columns"]:
+                if col["name"] == row_name:
+                    found_col = True
+                    assert col["description"] == row[6]
+                    break
+            assert found_col
 
-    #template_sql = Template(test_model_sql)
-    #fields = {"source": source}
-    #query = template_sql.render(**fields)
-    #query = query + " LIMIT 1;"
+    # Convert SQL to Jinja2 Template and replace function
+    def source(schema, table):
+        return f"{schema}.{table}"
 
-    ## Execute and validate query
-    #with conn.cursor() as cursor:
-    #    cursor.execute(query)
-    #    result = cursor.fetch_pandas_all()
-    #
-    ## Validate columns
-    #with open(metadata_file, "r") as file:
-    #    metadata_csv = csv.reader(file, delimiter=",")
-    #    next(metadata_csv)
-    #    for row in metadata_csv:
-    #        if row[4] != "":
-    #            row_name = row[4]
-    #        else: 
-    #            row_name = row[3]
-    #        assert row_name.upper() in list(result.columns)
+    template_sql = Template(test_model_sql)
+    fields = {"source": source}
+    query = template_sql.render(**fields)
+    query = query + " LIMIT 1;"
 
-    ## Validate quantity
-    #assert len(result) == 1
+    # Execute and validate query
+    query_job = client.query(query)
+    query_job.result()
+    assert query_job.errors == None
+    
+    rows = []
+
+    for row in query_job:
+        rows.append(dict(row))
+
+    # Validate quantity
+    assert len(rows) == 1
+
+    columns = list(rows[0].keys())
+
+    # Validate columns
+    with open(metadata_file, "r") as file:
+        metadata_csv = csv.reader(file, delimiter=",")
+        next(metadata_csv)
+        for row in metadata_csv:
+            if row[4] != "":
+                row_name = row[4]
+            else: 
+                row_name = row[3]
+            assert row_name.upper() in list(columns)
 
 
 # Finalizers, clean up
@@ -199,7 +209,7 @@ def cleanup(request):
         shutil.rmtree("models", ignore_errors=True)
     def delete_test_model():
         # Delete test model
-        job_query = client.query(f"DROP TABLE {dataset_id}.{test_table}")
+        job_query = client.query(f"DROP TABLE {dataset_id}.{test_table};")
         job_query.result()
         assert job_query.errors == None
         # Delete dataset
