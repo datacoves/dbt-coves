e3165a3a33d144f8e2498bf5bd3d7044c8af627a
Sebastian Sassi <sebastian@datacoves.com>
2023-05-04 19:55:18 -0300

    Fix secrets manager handling.

--- dbt_coves/tasks/load/base.py
+++ dbt_coves/tasks/load/base.py
@@ -30,24 +30,26 @@ class BaseLoadTask(NonDbtBaseTask):
         return jsons
 
     def _load_secret_data(self) -> dict:
-        # Contact the manager and retrieve Secrets
-        secrets_url = os.getenv("DBT_COVES_SECRETS_URL") or self.get_config_value("secrets_url")
-        secrets_token = os.getenv("DBT_COVES_SECRETS_TOKEN") or self.get_config_value(
-            "secrets_token"
-        )
-        if not (secrets_url and secrets_token):
-            raise LoadException(
-                "[b]secrets_url[/b] and [b]secrets_token[/b] must be provided"
-                "when using a Secrets Manager"
-            )
         payload = {}
-        if self.secrets_manager.lower() == "datacoves":
-            secrets_project = self.get_config_value("secrets_project")
-            if not secrets_project:
+        manager = self.secrets_manager.lower()
+        if manager == "datacoves":
+            # Contact the secrets manager and retrieve Secrets
+            secrets_url = os.getenv("DATACOVES__SECRETS_URL") or self.get_config_value(
+                "secrets_url"
+            )
+            secrets_token = os.getenv("DATACOVES__SECRETS_TOKEN") or self.get_config_value(
+                "secrets_token"
+            )
+            secrets_project = os.getenv("DATACOVES__SECRETS_PROJECT") or self.get_config_value(
+                "secrets_project"
+            )
+
+            if not (secrets_url and secrets_token and secrets_project):
                 raise LoadException(
-                    "[b]secrets_project[/b] must be provided when using 'datacoves'"
-                    "as your Secrets Manager "
+                    "[b]secrets_url[/b], [b]secrets_project[/b] and [b]secrets_token[/b] must "
+                    "be provided when using a Secrets Manager"
                 )
+
             secrets_url = f"{secrets_url}/api/v1/secrets/{secrets_project}"
             secrets_tags = self.get_config_value("secrets_tags")
             secrets_key = self.get_config_value("secrets_key")
@@ -55,8 +57,9 @@ class BaseLoadTask(NonDbtBaseTask):
                 payload["tags"] = set(secrets_tags)
             if secrets_key:
                 payload["key"] = secrets_key
+            headers = {"Authorization": f"token {secrets_token}"}
+            response = requests.get(secrets_url, headers=headers, verify=False, params=payload)
+            response.raise_for_status()
+            return response.json()
 
-        headers = {"Authorization": f"token {secrets_token}"}
-        response = requests.get(secrets_url, headers=headers, verify=False, params=payload)
-        response.raise_for_status()
-        return response.json()
+        raise LoadException(f"'{manager}' not recognized as a valid secrets manager.")
