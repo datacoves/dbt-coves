eeafc6938dd8fb78c67da9f6622b98bf83a4cccb
Bruno Antonellini <antonellinibruno@gmail.com>
2023-11-13 11:14:50 -0300

    Solve pre-commit flake8 issue

--- dbt_coves/tasks/generate/airflow_generators/base.py
+++ dbt_coves/tasks/generate/airflow_generators/base.py
@@ -157,7 +157,6 @@ class BaseDbtCovesTaskGenerator:
         Receive task_id, connection_id and Operator-string
         Returns Airflow call as a string
         """
-
         func_call = ", ".join(
             f'{k}="{v}"' if isinstance(v, str) else f"{k}={v}" for k, v in kwargs.items()
         )
