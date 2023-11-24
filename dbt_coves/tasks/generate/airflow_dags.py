8164a7f1eba487ed7b781172742533f36b995dcd
Bruno Antonellini <antonellinibruno@gmail.com>
2023-11-24 13:23:29 -0300

    Extend dbt-coves generate docs with PROFILES_DIR (#419)
    * Extend dbt-coves generate docs with PROFILES_DIR
    
    * Fix generate airflow-dags

--- dbt_coves/tasks/generate/airflow_dags.py
+++ dbt_coves/tasks/generate/airflow_dags.py
@@ -303,7 +303,7 @@ class GenerateAirflowDagsTask(NonDbtBaseTask):
                 elif isinstance(task_call, dict):
                     trigger = task_call.pop("trigger")
                     sensor = task_call.pop("sensor")
-                    task_group_output.append(f"{' ' *8}{trigger['name']}\n")
+                    task_group_output.append(f"{' ' *8}{trigger['call']}\n")
                     task_group_output.append(f"{' ' *8}{sensor['call']}\n")
                     task_group_output.append(f"{' ' *8}{trigger['name']} >> {sensor['name']}\n")
 
