5aa5390fe47cf2e8fefa8503f06aa8bd1150f0cd
Bruno Antonellini <antonellinibruno@gmail.com>
2024-02-08 08:27:31 -0300

    feat: trigger dbt-coves build and publish

--- dbt_coves/core/main.py
+++ dbt_coves/core/main.py
@@ -32,8 +32,8 @@ try:
 except ImportError:
     from dbt.cli.resolvers import default_profiles_dir
 
-    PROFILES_DIR = os.environ.get("DBT_PROFILES_DIR") or default_profiles_dir()
     VARS_DEFAULT_IS_STR = True
+    PROFILES_DIR = os.environ.get("DBT_PROFILES_DIR") or default_profiles_dir()
 
 console = Console()
 
