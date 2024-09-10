ea48aac14600e80267bb5a0774186566a2058cfd
BAntonellini <BAntonellini@users.noreply.github.com>
2024-09-10 18:28:59 +0000

    Bump version: 1.8.6 → 1.8.7

--- dbt_coves/__init__.py
+++ dbt_coves/__init__.py
@@ -1,6 +1,6 @@
 import dbt.version
 
-__version__ = "1.8.6"
+__version__ = "1.8.7"
 __dbt_major_version__ = int(dbt.version.installed.major or 0)
 __dbt_minor_version__ = int(dbt.version.installed.minor or 0)
 __dbt_patch_version__ = int(dbt.version.installed.patch or 0)
